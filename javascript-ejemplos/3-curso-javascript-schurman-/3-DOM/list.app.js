b6153e70e80f62c14bd26f9fed88c7fa24ad751c
Miguel Pantigoso <josepantigoso@upeu.edu.pe>
2020-09-20 20:39:08 -0500

    change windows to linux

--- javascript-ejemplos/3-curso-javascript-schurman-/3-DOM/list.app.js
+++ javascript-ejemplos/3-curso-javascript-schurman-/3-DOM/list.app.js
@@ -1,11 +1,13 @@
 window.onload = () => {
-  const formulario = document.getElementById('todo-form')
+    const formulario = document.getElementById('todo-form')
 
-  formulario.onsubmit=(e)=>{
-    e.preventDefault();
-    const todo = document.getElementById('todo')
-    const todoText = todo.value;
-    todo.value= '';
-    console.log(todoText);
-  }
+    formulario.onsubmit = (e) => {
+        e.preventDefault();
+        const todo = document.getElementById('todo')
+        const todoText = todo.value;
+        todo.value = '';
+        console.log(todoText);
+    }
+
+    function valor(){}   
 }
