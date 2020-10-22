c2a2174478da0814706ce028bef813061ccdc8b5
josmig <pantigosomiguel59@gmail.com>
2020-10-21 23:09:54 -0500

    reset

---
+++ curso-guia-javascript/1.Strings/concatenar_string.js
@@ -0,0 +1,8 @@
+//Concatenar Strings
+ const producto = "Monitor 20 pulgadas"
+ const precio = ' 30 USD'
+
+//Forma de concatenar sin el + 
+ console.log(producto.concat(precio));
+ console.log(producto.concat("En descuento"));
+
