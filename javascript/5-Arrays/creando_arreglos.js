0c0707990f0da40c8f9520153a3957fc13065c36
josmig <pantigosomiguel59@gmail.com>
2020-12-02 18:53:53 -0500

    Uso de arrays

---
+++ javascript/5-Arrays/creando_arreglos.js
@@ -0,0 +1,9 @@
+//Creacion de un array
+//Los arrays comienzan desde la posicion 0 
+const numeros = [10,02,30,20,102,23]
+const meses = new Array('Enero','Febrero','Marzo')
+
+//Arreglo con todo tipo de dato y multiples elementos
+const deTodo = ['Miguel',20,21.9,true,{nombre: 'Juan', trabajo:'programador'}]
+console.log(deTodo);
+console.log(numeros[3]);
