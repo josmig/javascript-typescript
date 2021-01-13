5ba27a4053fafb36593f4abb886ecd3780e079a2
josmig <pantigosomiguel59@gmail.com>
2021-01-13 19:26:41 +0200

    arrow function

---
+++ javascript/6-Funciones/7-ArrowFunction.js
@@ -0,0 +1,12 @@
+//Function normal
+const aprendiendo = function(){
+    console.log('Aprendiendo Javascript');
+}
+aprendiendo()
+//Creando Arrow Function
+const aprendiendo2 = () => console.log('Aprendiendo Javascript dos!')
+aprendiendo2()
+
+//Pasando parametos a un arrow function
+const ejemplo = (tecnologia,tec2) => console.log(`Aprendiendo ${tecnologia} y ${tec2}`)
+ejemplo('Javascript','Node.js')
