d05728255f23ac5b617c343c108543aa73125a4d
josmig <pantigosomiguel59@gmail.com>
2021-03-10 22:47:04 -0500

    Conociendo Typescript - tipo de datos

---
+++ typescript-2021/1-tipo_datos/2-tipo_number.ts
@@ -0,0 +1,8 @@
+
+function calcularArea(side: number){
+    console.log(side*side);
+}
+//con esto especificamos que tipo de dato sera esa variable (muy diferente a var,let ,const)
+const squareSide:number = 8;
+
+calcularArea(squareSide)
